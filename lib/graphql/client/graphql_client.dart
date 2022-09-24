54a2574f2e8989ec0a5c137af9f25b8f3e8ea080
HasegawaShuhei <sh1027.dev@gmail.com>
2022-09-25 00:00:36 +0900

    graphql_client追加

---
+++ lib/graphql/client/graphql_client.dart
@@ -0,0 +1,25 @@
+import 'package:ferry/ferry.dart' as ferry;
+import 'package:ferry_hive_store/ferry_hive_store.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final graphqlClientProvider =
+    Provider<ferry.Client>((ref) => throw UnimplementedError());
+
+Future<ferry.Client> initClient() async {
+  await Hive.initFlutter();
+
+  final box = await Hive.openBox<ferry.Cache>('graphql');
+
+  final store = HiveStore(box);
+
+  final cache = ferry.Cache(store: store);
+
+  final link = HttpLink('https://pokeapi.dev/');
+
+  return ferry.Client(
+    link: link,
+    cache: cache,
+  );
+}
